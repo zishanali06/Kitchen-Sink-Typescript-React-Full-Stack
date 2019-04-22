16817cd96bbdd832b6de8fb6b703be91a5046cdc
Zishan Ali <zishanali06@gmail.com>
2019-04-22 12:42:54 -0500

    working on homepage

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -16,20 +16,19 @@ class Navbar extends React.Component<NavbarProps, NavbarState> {
     }
     render() { 
         return ( 
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
-            <span className="navbar-brand mb-0 h1">Chirper</span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-secondary shadow">
+            <span className="navbar-brand mb-0 h1">ZtheDon Blog</span>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav ml-auto">
-                    <NavLink to="/" className="nav-item nav-link" exact activeClassName="nav-item nav-link active">Chirps</NavLink>
-                    <NavLink to="/magic" className="nav-item nav-link" exact activeClassName="nav-item nav-link active">Register/Login</NavLink>
+                    <NavLink to="/" className="nav-item nav-link" exact activeClassName="nav-item nav-link active">Blog</NavLink>
                 </div>
             </div>
         </nav>
         );
     }
 }
- 
+
 export default Navbar;
--- src/client/views/Bloghome.tsx
+++ src/client/views/Bloghome.tsx
@@ -1,23 +1,308 @@
 import * as React from 'react';
 
 export interface BloghomeProps {
-    
+
 }
- 
+
 export interface BloghomeState {
-    
+
 }
- 
+
 class Bloghome extends React.Component<BloghomeProps, BloghomeState> {
     constructor(props: BloghomeProps) {
         super(props);
-        this.state = { };
+        this.state = {};
     }
-    render() { 
-        return ( 
-            <h1>Testing123</h1>
+    render() {
+        return (
+            <main className="container">
+                <section className="row d-flex justify-content-center align-items-center bg1 text-center text-white">
+                    <h1 className="col-12 mt-5 tshadow">JavaScript done right.</h1>
+                    <button className="btm btn-outline-danger mb-5 mr-3">I need a website</button>
+                    <button className="btm btn-outline-danger mb-5 ml-3">I'm looking to hire</button>
+                </section>
+                <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                </section>
+                <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                </section>
+                <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+                    <section className="row">
+                    <h1>testing 123</h1>
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <br />
+                    <p>askdfasdf</p>
+                    <p>askdfasdf</p></section>
+
+            </main>
         );
     }
 }
- 
+
 export default Bloghome;
