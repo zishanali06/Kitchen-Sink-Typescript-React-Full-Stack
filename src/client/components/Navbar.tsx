2a4d09e09d544eba439b314e7d7db2a6df133255
Zishan Ali <zishanali06@gmail.com>
2019-04-21 21:17:42 -0500

    added navbar

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export interface NavbarProps {
     
@@ -15,7 +16,18 @@ class Navbar extends React.Component<NavbarProps, NavbarState> {
     }
     render() { 
         return ( 
-            <h1>Testing</h1>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+            <span className="navbar-brand mb-0 h1">Chirper</span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav ml-auto">
+                    <NavLink to="/" className="nav-item nav-link" exact activeClassName="nav-item nav-link active">Chirps</NavLink>
+                    <NavLink to="/magic" className="nav-item nav-link" exact activeClassName="nav-item nav-link active">Register/Login</NavLink>
+                </div>
+            </div>
+        </nav>
         );
     }
 }
